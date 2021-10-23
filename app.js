e774646e1e82e5a680026468651f2ba12f8fd25d
Kendra <kendrawashington@users.noreply.github.com>
2021-10-23 14:22:34 -0400

    Show and Hide New User Form

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 //UI Variables
-const newUser = document.getElementById("newUser")
+const newUser = document.getElementById("newUser");
+const getStarted = document.getElementById('getStarted');
 
 //Event Listeners
 newUser.addEventListener('click', saveUserData);
+getStarted.addEventListener('click', showForm)
     
 
 //User Array
 let users = [];
 
 //HELPER FUNCTIONS
-//Display Loading wheel
+//Show Form
+function showForm(e){
+    var x = document.getElementById('newUserForm')
+    x.style.display = "block"
+}
+
+function hideForm(e){
+    e.preventDefault()
+    var x = document.getElementById('newUserForm')
+    x.style.display = "none"
+}
+//Save User Data
 function saveUserData(e){
     e.preventDefault();
-    let user = {
-        id: Date.now(),
-        email: document.getElementById('exampleInputEmail1').value,
-        password: document.getElementById('exampleInputPassword1').value
+    if (document.getElementById('exampleInputEmail1').value != ""){
+        let user = {
+            id: Date.now(),
+            email: document.getElementById('exampleInputEmail1').value,
+            password: document.getElementById('exampleInputPassword1').value,
+            hairType: document.getElementById('hairType').value,
+            shampoo: document.getElementById('shampoo').value,
+            conditioner: document.getElementById('conditioner').value,
+            otherProducts: document.getElementById('otherProducts').value
+        }
+        users.push(user);
+        document.forms[0].reset();
     }
-    users.push(user);
-    document.forms[0].reset();
+    
+    hideForm(e);
 
     //saving to localStorage
     localStorage.setItem('MyUserList', JSON.stringify(users) );
